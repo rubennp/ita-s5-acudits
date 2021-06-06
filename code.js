416873329002f84046aa3ef239c0d6a92bca7530
Rubèn Nieto <ruben.np@me.com>
2021-06-06 21:15:00 +0200

    Fi nivell 2

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
-let joke;
-
 const getJoke = async () => {
     return await new Promise((resolve, reject) => {
         fetch('https://icanhazdadjoke.com/', {
@@ -13,10 +11,33 @@ const getJoke = async () => {
     });
 };
 
+const getWeather = async () => {
+    return await new Promise((resolve, reject) => {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&lang=ca&appid=5cabe7ed609895d6bb216b752535a455', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+        .then(r => r.json())
+        .then(d => resolve(d.weather[0].description.firstUpper()));
+    });
+};
+
 const newJoke = () => {
     getJoke().then((ok) => {
-        joke = ok;
-        document.getElementById("joke").innerHTML = joke;
-        console.log(joke);
+        document.getElementById("joke").innerHTML = ok;
+        console.log(ok);
     });
+};
+
+window.addEventListener('load', () => {
+    getWeather().then((ok) => {
+        document.getElementById("temps").innerHTML = ok;
+        console.log(ok);
+    });
+});
+
+String.prototype.firstUpper = function() {
+    return `${this.charAt(0).toUpperCase()}${this.substring(1)}`;
 };
