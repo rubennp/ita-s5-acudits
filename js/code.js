197706db8165e1058e0c716b3a2d114140febdb0
Rubèn Nieto <ruben.np@me.com>
2021-06-07 11:18:30 +0200

    Buscat altres apis d'acudits

--- js/code.js
+++ js/code.js
@@ -1,3 +1,19 @@
+const jokeSources = 
+[
+    {
+        url: 'https://icanhazdadjoke.com/',
+        dataKeys: ['joke'],
+    },
+    {
+        url: 'https://api.chucknorris.io/jokes/random',
+        dataKeys: ['value'],
+    },
+    {
+        url: 'https://official-joke-api.appspot.com/jokes/random',
+        dataKeys: ['setup', 'punchline'],
+    },
+];
+
 const getJoke = async () => {
     return await new Promise((resolve, reject) => {
         fetch('https://icanhazdadjoke.com/', {
@@ -39,3 +55,31 @@ window.addEventListener('load', () => {
 String.prototype.firstUpper = function() {
     return `${this.charAt(0).toUpperCase()}${this.substring(1)}`;
 };
+
+//chuck norris
+fetch("https://api.chucknorris.io/jokes/random", {
+	"method": "GET",
+	"headers": {
+		"accept": "application/json",
+	}
+})
+.then(r => r.json())
+.then(d => d.value)
+.then(ok => console.log(ok))
+.catch(err => {
+	console.error(err);
+});
+
+//jokes api
+fetch("https://official-joke-api.appspot.com/jokes/random", {
+	"method": "GET",
+	"headers": {
+		"accept": "application/json",
+	}
+})
+.then(r => r.json())
+.then(d => `${d.setup} ${d.punchline}`)
+.then(ok => console.log(ok))
+.catch(err => {
+	console.error(err);
+});
